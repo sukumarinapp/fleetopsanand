4a47d23cf44ce9c09da3353c5d853a3a7660c3ef
sukumarinapp <sukumar.inapp@gmail.com>
2021-07-30 20:13:06 +0530

    fix

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -161,10 +161,9 @@ class VehicleController extends Controller
             }
 
             $ECY = trim($request->get('ECY'));
-            $CON = Formulae::fuel_consumption($ECY);
-
-            $vehicle = Vehicle::find($id);
             $vehicle->VNO  =  $request->get('VNO');
+            $CON = Formulae::CON($vehicle->VNO);
+            $vehicle = Vehicle::find($id);            
             $vehicle->CAN  =  $request->get('CAN');
             if($VID != "") $vehicle->VID  =  $VID;
             if($VRD != "") $vehicle->VRD  =  $VRD;
