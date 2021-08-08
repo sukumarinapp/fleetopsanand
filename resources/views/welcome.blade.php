eea8b08d2017f5c1c8a9beb97dc87424d371759e
sukumarinapp <sukumar.inapp@gmail.com>
2021-08-08 13:20:15 +0530

    add sql

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <img src="logo.jpg" width="50%">
+                    <img src="logo.jpg" width="30%">
                 </div>
             </div>
         </div>
