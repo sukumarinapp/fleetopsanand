c2ce27d71a99e8a85c00d674ac87dfe15c70d860
universejino <universejino@gmail.com>
2021-08-11 16:31:07 +0530

    add menu

--- resources/views/manager/edit.blade.php
+++ resources/views/manager/edit.blade.php
@@ -209,7 +209,7 @@
                  </div>
         </div>
         <div class="modal-footer">
-              <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary" data-dismiss="modal">Confirm</button>
         </div>
     </div>
             </form>
