da64363e5fc64b980a0a99dfdbb6e468d856ad1d
sukumarinapp <sukumar.inapp@gmail.com>
2021-08-13 16:00:27 +0530

    add driver name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,10 @@
             <h3 class="">Dashboard</h3>
           </div>
    <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
+          <!-- <div class="col-lg-3 col-6">
             <div class="small-box bg-info">
               <div class="inner">
                 <h3>150</h3>
-
                 <p>New Orders</p>
               </div>
               <div class="icon">
@@ -18,14 +16,11 @@
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
+          </div> -->
+          <!-- <div class="col-lg-3 col-6">
             <div class="small-box bg-success">
               <div class="inner">
                 <h3>53<sup style="font-size: 20px">%</sup></h3>
-
                 <p>Bounce Rate</p>
               </div>
               <div class="icon">
@@ -33,14 +28,11 @@
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
+          </div> -->
+         <!--  <div class="col-lg-3 col-6">
             <div class="small-box bg-warning">
               <div class="inner">
                 <h3>44</h3>
-
                 <p>User Registrations</p>
               </div>
               <div class="icon">
@@ -48,14 +40,11 @@
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
+          </div> -->
+          <!-- <div class="col-lg-3 col-6">
             <div class="small-box bg-danger">
               <div class="inner">
                 <h3>65</h3>
-
                 <p>Unique Visitors</p>
               </div>
               <div class="icon">
@@ -63,8 +52,7 @@
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-          </div>
-          <!-- ./col -->
+          </div> -->
         </div>
           <div class="card bg-gradient">
               <div class="card-header border-0">
