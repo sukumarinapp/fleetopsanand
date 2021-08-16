ef20deffc060250578e1bd81727544f5576a6cfc
sukumarinapp <sukumar.inapp@gmail.com>
2021-08-16 17:33:47 +0530

    fix workflow grid

--- resources/views/workflow.blade.php
+++ resources/views/workflow.blade.php
@@ -7,7 +7,7 @@
 				<div class="card-header">
 					<h3 class="card-title">WorkFlow</h3>
 				</div>
-				<div class="card-body">
+				<div class="card-body" style="overflow-x: auto;" >
 						<table id="example1" class="table table-bordered table-striped">
           <thead>
           <tr>
