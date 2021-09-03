ea54b3cd59335df9eff80f1430122e0b1187c2b8
sukumarinapp <sukumar.inapp@gmail.com>
2021-09-03 11:37:33 +0530

    fix tree

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -167,6 +167,8 @@
 
 <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.11.0/js/dataTables.bootstrap4.min.js"></script>
+<script type="text/javascript" language="javascript" src="js/hummingbird-treeview.js"></script>
+
     <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
     <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
     <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
@@ -176,9 +178,10 @@
     <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
     <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.colVis.min.js"></script>
 @stack('page_scripts')
-<script type="text/javascript" language="javascript" src="js/hummingbird-treeview.js"></script>
 <script>
 $(document).ready(function(){
+    $("#treeview").hummingbird();
+
     $(".alert-success").fadeTo(2000, 500).slideUp(500, function(){
         $(".alert-success").slideUp(500);
     });
