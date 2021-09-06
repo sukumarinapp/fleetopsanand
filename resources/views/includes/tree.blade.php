7f6ad5323cfaa1d345022bd57c212859e247f196
sukumarinapp <sukumar.inapp@gmail.com>
2021-09-06 17:54:04 +0530

    bug fix

--- resources/views/includes/tree.blade.php
+++ resources/views/includes/tree.blade.php
@@ -6,69 +6,51 @@
       <input id="xnode-0" data-id="custom-{{ Auth::user()->id }}" type="checkbox" /> {{ Auth::user()->name }}
     </label>
     <ul>
-      @foreach($usertree as $manager)
-      <li data-id="1">
+      @foreach($usertree as $key => $manager)
+      <li data-id="{{ $key+1 }}">
         <i class="fa fa-plus"></i>
         <label>
-          <input  id="xnode-0-1" data-id="custom-0-1" type="checkbox" />{{ $manager['name'] }} {{ $manager['UZS'] }}
+          <input  id="xnode-0-1" data-id="custom-0-1" type="checkbox" /> {{ $manager['name'] }} {{ $manager['UZS'] }}
         </label>
         <ul>
-          @foreach($manager['client'] as $client)
-          <li data-id="2">
+          @foreach($manager['submanager'] as $key2 => $submanager)
+          <li data-id="{{ $key2+1 }}">
             <i class="fa fa-plus"></i>
             <label>
-              <input  id="xnode-0-1-2" data-id="custom-0-1-2" type="checkbox" />{{ $client['name'] }} {{ $client['UZS'] }}
-            </label>
-          </li>
-          <ul>
-            @foreach($client['vehicle'] as $vehicle)
-              <li>
-                <label>
-                  <input class="hummingbird-end-node" id="xnode-0-1-2-3" data-id="custom-0-1-2-3" type="checkbox" />{{ $vehicle['VNO'] }}
-                </label>
-              </li>
-            @endforeach   
-          </ul>
-          @endforeach
-        </ul>
-        <!-- <ul>
-          @foreach($manager['submanager'] as $submanager)
-          <li data-id="{{ $submanager['id'] }}">
-            <i class="fa fa-plus"></i>
-            <label>
-              <input  id="xnode-0-{{ $manager['id'] }}-{{ $submanager['id'] }}" data-id="custom-0-1-2" type="checkbox" />{{ $submanager['name'] }} {{ $submanager['UZS'] }}
+              <input  id="xnode-0-1-2" data-id="custom-0-1-2" type="checkbox" /> {{ $submanager['UZS'] }} {{ $submanager['name'] }}
             </label>
             <ul>
-              @foreach($submanager['client'] as $client)
-              <li data-id="{{ $client['id'] }}">
+              @foreach($submanager['client'] as $key3 => $client)
+              <li data-id="{{ $key3+1 }}">
                 <i class="fa fa-plus"></i>
                 <label>
-                  <input  id="xnode-0-{{ $manager['id'] }}-{{ $submanager['id'] }}-{{ $client['id'] }}" data-id="custom-0-1-2-3" type="checkbox" /> {{ $client['name'] }} {{ $client['UZS'] }}
+                  <input  id="xnode-0-1-2-3" data-id="custom-0-1-2-3" type="checkbox" /> {{ $client['name'] }} {{ $client['UZS'] }}
                 </label>
                 <ul>
-                  @foreach($client['vehicle'] as $vehicle)
-                  <li >
+                  @foreach($client['vehicle'] as $key4 => $vehicle)
+                  <li>
                     <label>
-                      <input class="hummingbird-end-node" id="xnode-0-{{ $manager['id'] }}-{{ $submanager['id'] }}-{{ $client['id'] }}-{{ $vehicle['id'] }}" data-id="custom-0-{{ $manager['id'] }}-{{ $submanager['id'] }}-{{ $client['id'] }}-{{ $vehicle['id'] }}" type="checkbox" />{{ $vehicle['VNO'] }}
+                      <input class="hummingbird-end-node" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" type="checkbox" /> {{ $vehicle['VNO'] }}
                     </label>
                   </li>
-                  @endforeach
+                  @endforeach 
                 </ul>
               </li>
               @endforeach    
             </ul>
           </li>                            
           @endforeach
-        </ul> -->
+        </ul>
       </li>
-      @endforeach    
+      @endforeach
     </ul>
   </li>
 </ul>
 </div>
 <ul>
-@foreach($usertree as $manager)
-  <li>{{ $manager['name'] }} {{ $manager['UZS'] }}:{{ $manager['usertype'] }}:{{ $manager['level'] }}</li>
+
+@foreach($usertree as $key => $manager)
+  <li>{{ $key+1 }} {{ $manager['name'] }} {{ $manager['UZS'] }}:{{ $manager['usertype'] }}:{{ $manager['level'] }}</li>
   <ul>
   @foreach($manager['client'] as $client)
     <li>{{ $client['name'] }} {{ $client['UZS'] }}:{{ $client['usertype'] }}:{{ $client['level'] }}</li>
