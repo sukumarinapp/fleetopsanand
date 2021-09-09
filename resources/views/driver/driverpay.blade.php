b78457ac384a6429f28613025afa37f799ee2f39
sukumarinapp <sukumar.inapp@gmail.com>
2021-09-09 15:01:25 +0530

    bug fix

--- resources/views/driver/driverpay.blade.php
+++ resources/views/driver/driverpay.blade.php
@@ -20,7 +20,7 @@
         @endphp    
             <div class="col-sm-12 col-md-4 text-center">
               <div class="custom-control custom-radio image-checkbox">
-                  <input {{ key == 0 ? "checked":"" }} value="{{ $item->provider }}" type="radio" class="custom-control-input" id="{{ $item->name }}" name="options">
+                  <input {{ $key == 0 ? "checked":"" }} value="{{ $item->provider }}" type="radio" class="custom-control-input" id="{{ $item->name }}" name="options">
                   <label class="custom-control-label" for="{{ $item->name }}">
                       <img width="120px" src="https://usebillbox.com/{{ $item->logo }}" alt="#" class="img-fluid">
                   </label>
