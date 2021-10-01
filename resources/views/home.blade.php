9a0729e1f7f9bcc6229afc50ef472c6dbc4b009f
sukumarinapp <sukumar.inapp@gmail.com>
2021-10-01 18:40:23 +0530

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,7 +87,7 @@
                         </button>
                       </div>
                       <div class="modal-body">
-                        <div id="map-canvas" style="width:300px;height: 300px;"></div>
+                        <div id="map-canvas" style="width:400px;height: 300px;"></div>
                       </div>
                       <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
