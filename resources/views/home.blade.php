812dd3734791c813e65f396d8c94acd782ea3d57
sukumarinapp <sukumar.inapp@gmail.com>
2021-10-01 16:01:17 +0530

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,8 +64,20 @@
                 {{ $alert["VNO"] }}</a></td>
                 <td>{{ $alert["alert"] }}</td>
                 <td>{{ round($alert["hours"],2) }}</td>
-                <td> <a href="#" data-toggle="modal" data-target="#exampleModal">{{ $alert["latitude"] }} {{ $alert["longitude"] }}</a></td>
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <td> <a href="#" onclick="showmap({{ $alert['latitude'] }},{{ $alert['longitude'] }})" >{{ $alert["latitude"] }} {{ $alert["longitude"] }}</a></td>
+                
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div> 
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                   <div class="modal-dialog">
                     <div class="modal-content">
                       <div class="modal-header">
@@ -83,22 +95,13 @@
                     </div>
                   </div>
                 </div>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
-        </div> 
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-</div>
-
 @endsection
 @section('third_party_scripts')
 
 <script>
+  function showmap(lat,long){
+    $('#exampleModal').modal('show');
+  }
   function check_checked(VNO){
     if($("."+VNO).is(':checked')){
       return true;
