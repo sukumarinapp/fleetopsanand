9fed1f05df6c045c76bd4db19a664071315977c1
universejino <universejino@gmail.com>
2021-10-05 12:12:16 +0530

    add

--- resources/views/includes/tree.blade.php
+++ resources/views/includes/tree.blade.php
@@ -1,137 +1,137 @@
 <div id="treeview_container" class="hummingbird-treeview" style="height:380px;overflow-x: auto;overflow-y: auto;">
-<ul style="padding-left: 0px;" id="treeview" class="hummingbird-base">
-  <li data-id="0">
-    <i class="fa fa-plus"></i>
-    <label>
-      <input id="xnode-0" data-id="custom-{{ Auth::user()->id }}" type="checkbox" /> {{ Auth::user()->name }}
-    </label>
-    @if($type == "admin")
-    <ul style="padding-left:15px">
-      @foreach($usertree as $key => $manager)
-      <li data-id="{{ $key+1 }}">
-        <i class="fa fa-plus"></i>
-        <label>
-          <input  id="xnode-0-1" data-id="custom-0-1" type="checkbox" /> {{ $manager['name'] }}
-        </label>
-        <ul style="padding-left:15px">
-          @foreach($manager['client'] as $key2 => $client)
-          <li data-id="{{ $key2+1 }}">
-            <i class="fa fa-plus"></i>
-            <label>
-              <input  id="xnode-0-1-2" data-id="custom-0-1-2" type="checkbox" /> {{ $client['name'] }}
-            </label>
-            <ul style="padding-left:15px">
-              @foreach($client['vehicle'] as $key3 => $vehicle)
-              <li>
-                <label>
-                  <input class="hummingbird-end-node {{ $vehicle['VNO'] }}" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" type="checkbox" value="{{ $vehicle['VNO'] }}" /> {{ $vehicle['VNO'] }}
-                </label>
-              </li>
-              @endforeach 
-            </ul>
-          </li>                            
-          @endforeach
-        </ul>
-        <ul style="padding-left:15px">
-          @foreach($manager['submanager'] as $key2 => $submanager)
-          <li data-id="{{ $key2+1 }}">
-            <i class="fa fa-plus"></i>
-            <label>
-              <input  id="xnode-0-1-2" data-id="custom-0-1-2" type="checkbox" /> {{ $submanager['name'] }}
-            </label>
-            <ul style="padding-left:15px">
-              @foreach($submanager['client'] as $key3 => $client)
-              <li data-id="{{ $key3+1 }}">
-                <i class="fa fa-plus"></i>
-                <label>
-                  <input  id="xnode-0-1-2-3" data-id="custom-0-1-2-3" type="checkbox" /> {{ $client['name'] }}
-                </label>
-                <ul style="padding-left:15px">
-                  @foreach($client['vehicle'] as $key4 => $vehicle)
-                  <li>
-                    <label>
-                      <input class="hummingbird-end-node {{ $vehicle['VNO'] }}" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" type="checkbox" value="{{ $vehicle['VNO'] }}" /> {{ $vehicle['VNO'] }}
-                    </label>
-                  </li>
-                  @endforeach 
-                </ul>
-              </li>
-              @endforeach    
-            </ul>
-          </li>                            
-          @endforeach
-        </ul>
-      </li>
-      @endforeach
-    </ul>
-    @endif
-
-    @if($type == "manager")
-    <ul style="padding-left:15px">
-      @foreach($usertree as $key1 => $submanager)
-      <li data-id="{{ $key1+1 }}">
-        <i class="fa fa-plus"></i>
-        <label>
-          <input  id="xnode-0-1" data-id="custom-0-1" type="checkbox" /> {{ $submanager['name'] }}
-        </label>
-        <ul style="padding-left:15px">
-          @foreach($submanager['client'] as $key2 => $client)
-          <li data-id="{{ $key2+1 }}">
-            <i class="fa fa-plus"></i>
-            <label>
-              <input  id="xnode-0-1-2" data-id="custom-0-1-2" type="checkbox" /> {{ $client['name'] }}
-            </label>
-            <ul style="padding-left:15px">
-              @foreach($client['vehicle'] as $key3 => $vehicle)
-              <li>
-                <label>
-                  <input class="hummingbird-end-node {{ $vehicle['VNO'] }}" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" type="checkbox" value="{{ $vehicle['VNO'] }}" /> {{ $vehicle['VNO'] }}
-                </label>
-              </li>
-              @endforeach 
-            </ul>
-          </li>                            
-          @endforeach
-        </ul>
-      </li>
-      @endforeach
-    </ul>
-    @endif
-
-    @if($type == "submanager")
+  <ul style="padding-left: 0px;" id="treeview" class="hummingbird-base">
+    <li data-id="0">
+      <i class="fa fa-plus"></i>
+      <label>
+        <input id="xnode-0" data-id="custom-{{ Auth::user()->id }}" type="checkbox" /> {{ Auth::user()->name }}
+      </label>
+      @if($type == "admin")
       <ul style="padding-left:15px">
-      @foreach($usertree as $key1 => $client)
-      <li data-id="{{ $key1+1 }}">
-        <i class="fa fa-plus"></i>
-        <label>
-          <input  id="xnode-0-1" data-id="custom-0-1" type="checkbox" /> {{ $client['name'] }}
-        </label>
-        <ul style="padding-left:15px">
-              @foreach($client['vehicle'] as $key3 => $vehicle)
-              <li>
-                <label>
-                  <input class="hummingbird-end-node {{ $vehicle['VNO'] }}" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" value="{{ $vehicle['VNO'] }}" type="checkbox" /> {{ $vehicle['VNO'] }}
-                </label>
-              </li>
-              @endforeach 
-            </ul>
-      </li>
-      @endforeach
-    </ul>
-    @endif
+        @foreach($usertree as $key => $manager)
+        <li data-id="{{ $key+1 }}">
+          <i class="fa fa-plus"></i>
+          <label>
+            <input  id="xnode-0-1" data-id="custom-0-1" type="checkbox" /> {{ $manager['name'] }}
+          </label>
+          <ul style="padding-left:15px">
+            @foreach($manager['client'] as $key2 => $client)
+            <li data-id="{{ $key2+1 }}">
+              <i class="fa fa-plus"></i>
+              <label>
+                <input  id="xnode-0-1-2" data-id="custom-0-1-2" type="checkbox" /> {{ $client['name'] }}
+              </label>
+              <ul style="padding-left:15px">
+                @foreach($client['vehicle'] as $key3 => $vehicle)
+                <li>
+                  <label>
+                    <input class="hummingbird-end-node {{ $vehicle['VNO'] }}" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" type="checkbox" value="{{ $vehicle['VNO'] }}" /> {{ $vehicle['VNO'] }}
+                  </label>
+                </li>
+                @endforeach 
+              </ul>
+            </li>                            
+            @endforeach
+          </ul>
+          <ul style="padding-left:15px">
+            @foreach($manager['submanager'] as $key2 => $submanager)
+            <li data-id="{{ $key2+1 }}">
+              <i class="fa fa-plus"></i>
+              <label>
+                <input  id="xnode-0-1-2" data-id="custom-0-1-2" type="checkbox" /> {{ $submanager['name'] }}
+              </label>
+              <ul style="padding-left:15px">
+                @foreach($submanager['client'] as $key3 => $client)
+                <li data-id="{{ $key3+1 }}">
+                  <i class="fa fa-plus"></i>
+                  <label>
+                    <input  id="xnode-0-1-2-3" data-id="custom-0-1-2-3" type="checkbox" /> {{ $client['name'] }}
+                  </label>
+                  <ul style="padding-left:15px">
+                    @foreach($client['vehicle'] as $key4 => $vehicle)
+                    <li>
+                      <label>
+                        <input class="hummingbird-end-node {{ $vehicle['VNO'] }}" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" type="checkbox" value="{{ $vehicle['VNO'] }}" /> {{ $vehicle['VNO'] }}
+                      </label>
+                    </li>
+                    @endforeach 
+                  </ul>
+                </li>
+                @endforeach    
+              </ul>
+            </li>                            
+            @endforeach
+          </ul>
+        </li>
+        @endforeach
+      </ul>
+      @endif
 
-    @if($type == "client")
+      @if($type == "manager")
       <ul style="padding-left:15px">
-      @foreach($usertree as $key1 => $vehicle)
-      <li>
-        <label>
-          <input class="hummingbird-end-node {{ $vehicle['VNO'] }}" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" type="checkbox" value="{{ $vehicle['VNO'] }}" /> {{ $vehicle['VNO'] }}
-        </label>
-      </li>             
-      @endforeach
-    </ul>
-    @endif
-  </li>
-</ul>
+        @foreach($usertree as $key1 => $submanager)
+        <li data-id="{{ $key1+1 }}">
+          <i class="fa fa-plus"></i>
+          <label>
+            <input  id="xnode-0-1" data-id="custom-0-1" type="checkbox" /> {{ $submanager['name'] }}
+          </label>
+          <ul style="padding-left:15px">
+            @foreach($submanager['client'] as $key2 => $client)
+            <li data-id="{{ $key2+1 }}">
+              <i class="fa fa-plus"></i>
+              <label>
+                <input  id="xnode-0-1-2" data-id="custom-0-1-2" type="checkbox" /> {{ $client['name'] }}
+              </label>
+              <ul style="padding-left:15px">
+                @foreach($client['vehicle'] as $key3 => $vehicle)
+                <li>
+                  <label>
+                    <input class="hummingbird-end-node {{ $vehicle['VNO'] }}" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" type="checkbox" value="{{ $vehicle['VNO'] }}" /> {{ $vehicle['VNO'] }}
+                  </label>
+                </li>
+                @endforeach 
+              </ul>
+            </li>                            
+            @endforeach
+          </ul>
+        </li>
+        @endforeach
+      </ul>
+      @endif
+
+      @if($type == "submanager")
+      <ul style="padding-left:15px">
+        @foreach($usertree as $key1 => $client)
+        <li data-id="{{ $key1+1 }}">
+          <i class="fa fa-plus"></i>
+          <label>
+            <input  id="xnode-0-1" data-id="custom-0-1" type="checkbox" /> {{ $client['name'] }}
+          </label>
+          <ul style="padding-left:15px">
+            @foreach($client['vehicle'] as $key3 => $vehicle)
+            <li>
+              <label>
+                <input class="hummingbird-end-node {{ $vehicle['VNO'] }}" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" value="{{ $vehicle['VNO'] }}" type="checkbox" /> {{ $vehicle['VNO'] }}
+              </label>
+            </li>
+            @endforeach 
+          </ul>
+        </li>
+        @endforeach
+      </ul>
+      @endif
+
+      @if($type == "client")
+      <ul style="padding-left:15px">
+        @foreach($usertree as $key1 => $vehicle)
+        <li>
+          <label>
+            <input class="hummingbird-end-node {{ $vehicle['VNO'] }}" id="xnode-0-1-2-3-1" data-id="custom-0-1-2-3-1" type="checkbox" value="{{ $vehicle['VNO'] }}" /> {{ $vehicle['VNO'] }}
+          </label>
+        </li>             
+        @endforeach
+      </ul>
+      @endif
+    </li>
+  </ul>
 </div>
 
--- resources/views/replay.blade.php
+++ resources/views/replay.blade.php
@@ -5,15 +5,44 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-12">
-          <div class="card">
+          <div class="card card-info">
+            <div class="card-header">
+              <h3 class="card-title">Replay</h3>
+            </div>
             <div class="card-body">
-              <h1>Replay</h1>
+             <div class="form-group row">
+               <label for="" class="col-sm-2 col-form-label">Track Replay</label>
+               <div class="col-sm-6">
+                 <select name="cars" required="required" class="form-control" id="cars">
+                  <optgroup label="Swedish Cars">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                  </optgroup>
+                  <optgroup label="German Cars">
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                  </optgroup>
+                </select>
+              </div>
+            <div class="col-sm-3">
+               <button type="button" class="btn btn-primary">Replay</button>
+            </div>
             </div>
           </div>
-        </div> 
-      </div>
-     </div> 
-  </div>
+        </div>
+      </div> 
+    </div>
+  </div> 
+</div>
 </div>
 @endsection
-
+<!-- @push('page_scripts')
+<script>
+  $(document).ready(function(){
+    $('.select2').select2({
+      theme: 'bootstrap4'
+    });
+  });
+</script>
+@endpush
+-->
