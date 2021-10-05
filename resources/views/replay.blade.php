00893d04d7c08693aab6f96c060ef76f6ee23a04
sukumarinapp <sukumar.inapp@gmail.com>
2021-10-05 12:31:07 +0530

    add database dump

--- resources/views/replay.blade.php
+++ resources/views/replay.blade.php
@@ -13,29 +13,41 @@
              <div class="form-group row">
                <label for="" class="col-sm-2 col-form-label">Track Replay</label>
                <div class="col-sm-6">
-                 <select name="cars" required="required" class="form-control" id="cars">
-                  <optgroup label="Swedish Cars">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                  </optgroup>
-                  <optgroup label="German Cars">
-                    <option value="mercedes">Mercedes</option>
-                    <option value="audi">Audi</option>
-                  </optgroup>
-                </select>
-              </div>
-            <div class="col-sm-3">
-               <button type="button" class="btn btn-primary">Replay</button>
-            </div>
-            </div>
-          </div>
-        </div>
-      </div> 
-    </div>
-  </div> 
-</div>
-</div>
-@endsection
+                 <select name="VNO" required="required" class="form-control selectpicker" id="VNO">
+                @if($type == "admin")
+                @foreach($usertree as $key => $manager)
+                <optgroup label="{{ $manager['name'] }}">
+                  @foreach($manager['client'] as $key2 => $client)
+                  <optgroup label="&nbsp;&nbsp;{{ $client['name'] }}">
+                    @foreach($client['vehicle'] as $key3 => $vehicle)
+                    <option value="{{ $vehicle['VNO'] }}">&nbsp;&nbsp;&nbsp;&nbsp;{{ $vehicle['VNO'] }}</option>
+                    @endforeach 
+                    @endforeach
+                    @foreach($manager['submanager'] as $key2 => $submanager)
+                    <optgroup label="&nbsp;&nbsp;{{ $submanager['name'] }}">
+                      @foreach($submanager['client'] as $key3 => $client)
+                      <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;{{ $client['name'] }}">
+                        @foreach($client['vehicle'] as $key4 => $vehicle)
+                        <option value="{{ $vehicle['VNO'] }}">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $vehicle['VNO'] }}</option>
+                      @endforeach 
+                    @endforeach    
+                        @endforeach
+                        @endforeach
+                        @endif
+                      </select>
+                    </div>
+                    <div class="col-sm-3">
+                     <button type="button" class="btn btn-primary">Replay</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div> 
+         </div>
+       </div> 
+     </div>
+   </div>
+   @endsection
 <!-- @push('page_scripts')
 <script>
   $(document).ready(function(){
