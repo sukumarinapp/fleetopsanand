b8eb37beb4b27d60c3902ee73e5ad14a06e5aa82
universejino <universejino@gmail.com>
2021-11-17 16:47:36 +0530

    change icon and reports

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,60 +1,61 @@
 @extends('layouts.app')
 @section('content')
 <style type="text/css">
-  @media screen and (min-width: 1000px) {  
-    .whatsappshare {  
-      display: none
-    }  
-  }
-  #speed{
-    color: red;
-    background: white;
-    padding: 10px;
-    border-radius: 10px;
-  }    
+@media screen and (min-width: 1000px) {  
+  .whatsappshare {  
+    display: none
+  }  
+}
+#speed{
+  color: red;
+  background: white;
+  padding: 10px;
+  border-radius: 10px;
+}    
 </style>
 <div class="container-fluid">
   <div class="content-header">
     <div class="container-fluid">
-      <!--  <div class="card card-success">
-              <div class="card-header">
+     <div class="card card-info">
+      <div class="card-header">
+        <h3 class="card-title">Tracker</h3>
 
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                  </button>
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool btn-secondary" data-card-widget="collapse"><i class="fas fa-minus"></i>
+          </button>
+        </div>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <div class="input-group mb-3">
+                  <input type="text" style="height:35px" id="search_inp" maxlength="15" onkeyup="search_tree(this)" onpaste="search_tree(this)" onchange="search_tree(this)" class="form-control" placeholder="Search" >&nbsp;
+                  <input id="toogle_button" onchange="toggle_map(this)"  type="checkbox" data-toggle="toggle" data-height="10" data-on="Normal" data-off="Replay" data-onstyle="success" data-offstyle="danger">
                 </div>
-              </div>
-              <div class="card-body"> -->
-      <div class="row">
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <div class="input-group mb-3">
-                <input type="text" style="height:35px" id="search_inp" maxlength="15" onkeyup="search_tree(this)" onpaste="search_tree(this)" onchange="search_tree(this)" class="form-control" placeholder="Search" >&nbsp;
-                <input id="toogle_button" onchange="toggle_map(this)"  type="checkbox" data-toggle="toggle" data-height="10" data-on="Normal" data-off="Replay" data-onstyle="success" data-offstyle="danger">
-              </div>
-              
-              @include("includes.tree")
-            </div>
-            <div class="col-md-12">
-              <div class="row">
-                <div class="col-sm-6" style="font-style: oblique; font-family: sans-serif;font-size: small; padding-bottom: 3px;">Vehicles Online: {{ $online }}</div>
-                <div class="col-sm-6" style="font-style: oblique;font-family: sans-serif; font-size: small;">Vehicles Offline: {{ $offline }}</div>
-              </div>
-              <div class="row">
-                <div class="col-sm-6" style="font-style: oblique;font-family: sans-serif;font-size: small; padding-bottom: 3px;">Vehicles Active: {{ $active }}</div>
-                <div class="col-sm-6" style="font-style: oblique;font-family: sans-serif;font-size: small;">Vehicles Inactive: {{ $inactive }}</div>
-              </div>
-              <div class="row">
-                <div class="col-sm-6" style="font-style: oblique;font-family: sans-serif;font-size: small;">Total Vehicles: {{ $total }}</div>
-              </div>
-              
-            </div>
-          </div>
-        </div> 
 
-        <div class="col-md-9">
-          <div class="card bg-gradient">
+                @include("includes.tree")
+              </div>
+              <div class="col-md-12">
+                <div class="row">
+                  <div class="col-sm-6" style="font-style: oblique; font-family: sans-serif;font-size: small; padding-bottom: 3px;">Vehicles Online: {{ $online }}</div>
+                  <div class="col-sm-6" style="font-style: oblique;font-family: sans-serif; font-size: small;">Vehicles Offline: {{ $offline }}</div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-6" style="font-style: oblique;font-family: sans-serif;font-size: small; padding-bottom: 3px;">Vehicles Active: {{ $active }}</div>
+                  <div class="col-sm-6" style="font-style: oblique;font-family: sans-serif;font-size: small;">Vehicles Inactive: {{ $inactive }}</div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-6" style="font-style: oblique;font-family: sans-serif;font-size: small;">Total Vehicles: {{ $total }}</div>
+                </div>
+
+              </div>
+            </div>
+          </div> 
+
+          <div class="col-md-9">
+            <div class="card bg-gradient">
               <!--
 
                 <div class="card-tools">
@@ -70,47 +71,57 @@
                 <form class="row form" >
                   @csrf
                   <div class="col-md-1">
-                      <label for="email">Start Time</label>                    
+                    <label for="email">Start Time</label>                    
                   </div>
                   <div class="col-md-2">
-                      <input value="{{ $starttime }}" class="form-control" type="datetime-local" name="starttime" id="starttime"  required="required">
+                    <input value="{{ $starttime }}" class="form-control" type="datetime-local" name="starttime" id="starttime"  required="required">
                   </div>
                   <div class="col-md-1">
-                      <label for="email">End Time</label>                    
+                    <label for="email">End Time</label>                    
                   </div>
                   <div class="col-md-2">
-                      <input value="{{ $endtime }}" class="form-control" type="datetime-local" name="endtime" id="endtime"  required="required">
+                    <input value="{{ $endtime }}" class="form-control" type="datetime-local" name="endtime" id="endtime"  required="required">
                   </div>
                   <div class="col-md-1">
-                      <input type="button" onclick="replaydata()"  value ="Play" class="btn btn-success btn-sm form-control">
+                    <input type="button" onclick="replaydata()"  value ="Play" class="btn btn-success btn-sm form-control">
                   </div>
                   <div class="col-md-1">
-                      <input type="button" onclick="pause()" value ="Pause" class="btn btn-danger btn-sm form-control">
+                    <input type="button" onclick="pause()" value ="Pause" class="btn btn-danger btn-sm form-control">
                   </div>
                   <div class="col-md-1">
-                      <input type="button" onclick="reset()" value ="Reset" class="btn btn-primary btn-sm form-control">
+                    <input type="button" onclick="reset()" value ="Reset" class="btn btn-primary btn-sm form-control">
                   </div>
                   <div class="col-md-1">
-                      <input type="button" onclick="fast()" name="submit"  value ="Fast" class="btn btn-primary btn-sm form-control">
+                    <input type="button" onclick="fast()" name="submit"  value ="Fast" class="btn btn-primary btn-sm form-control">
                   </div>
                   <div class="col-md-1">
-                      <span id="speed">1x</span>
+                    <span id="speed">1x</span>
                   </div>
                   <div class="col-md-1">
-                      <input type="button" onclick="slow()" name="submit"  value ="Slow" class="btn btn-primary btn-sm form-control">
+                    <input type="button" onclick="slow()" name="submit"  value ="Slow" class="btn btn-primary btn-sm form-control">
                   </div>
 
                 </form>
                 <div class="bg-danger" id="replay-summary" ></div>
-                <div id="replay-canvas" style="height: 460px"></div>
+                <div id="replay-canvas" style="height: 400px"></div>
               </div>
             </div>
           </div>
         </div> 
-         </div>
-      <div class="card">
-        <div class="card-body">
-         <div class="table-responsive" style="height:150px" >
+      </div>
+    </div>
+  </div>
+  <div class="card card-info">
+    <div class="card-header">
+      <h3 class="card-title">Alert</h3>
+
+      <div class="card-tools">
+        <button type="button" class="btn btn-tool btn-secondary" data-card-widget="collapse"><i class="fas fa-minus"></i>
+        </button>
+      </div>
+    </div>
+    <div class="card-body">
+         <div class="table-responsive">
           <table id="examplegrid" class="table table-bordered table-striped" style="overflow-y:auto; padding-bottom: 0; height: 100px; ">
             <thead style="width:70%">
               <tr>
@@ -129,8 +140,8 @@
                 <td><a data-toggle="popover" data-html="true" title="{{ $alert['VMK'] }} {{ $alert['VMD'] }} {{ $alert['VCL'] }}({{ $alert['VBM'] }})" data-content="<b>Customer:</b> {{ $alert['client'] }}<br /><b>Manager:</b> {{ $alert['manager'] }}<br /><b>Driver:</b> {{ $alert['driver'] }}" data-trigger="hover">
                 {{ $alert["VNO"] }}</a></td>
                 <td>
-                <img src="{{ $alert['icon'] }}" />
-                {{ $alert["alert"] }}
+                  <img src="{{ $alert['icon'] }}" />
+                  {{ $alert["alert"] }}
                 </td>
                 <td>{{ $alert["hours"] }}</td>
                 <td> <button type="button"
@@ -144,9 +155,10 @@
             </tbody>
           </table>
         </div> 
-      </div>
     </div>
   </div>
+</div>
+</div>
 </div>
 </div>
 </div>
@@ -172,155 +184,155 @@
 @section('third_party_scripts')
 
 <script>
-var map2;
+  var map2;
   function initialize2(myCenter) {
     var marker2 = new google.maps.Marker({
       position: myCenter
     });
     var mapProp2 = {
-          center: myCenter,
-          zoom: 16,
+      center: myCenter,
+      zoom: 16,
           //draggable: false,
           //scrollwheel: false,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         map2 = new google.maps.Map(document.getElementById("map-canvas"), mapProp2);
         marker2.setMap(map2);
-  };
+      };
 
-  $('#myMapModal').on('shown.bs.modal', function(e) {
-    var element = $(e.relatedTarget);
-    var data = element.data("lat").split(',')
-    initialize2(new google.maps.LatLng(data[0], data[1]));
-  });
+      $('#myMapModal').on('shown.bs.modal', function(e) {
+        var element = $(e.relatedTarget);
+        var data = element.data("lat").split(',')
+        initialize2(new google.maps.LatLng(data[0], data[1]));
+      });
 
-function check_checked(VNO){
-  if($("."+VNO).is(':checked')){
-    return true;
-  }else{
-    return false;
-  }
-}  
-var mapnormal;
-var markers = []; 
-var vehicles = [];
-function setMarkers(locations) {
-    for (var i = 0; i < locations.length; i++) {
-        var vehicle = locations[i];
-        var acc = "";
-        var engine_on = parseInt(vehicle["engine_on"]);
-        if(engine_on == 0){
-          acc = "ACC OFF";
+      function check_checked(VNO){
+        if($("."+VNO).is(':checked')){
+          return true;
         }else{
-          acc = "ACC ON";
+          return false;
         }
-        var car_icon = "off0.png";
-        var dir = parseFloat(vehicle['direction']);
+      }  
+      var mapnormal;
+      var markers = []; 
+      var vehicles = [];
+      function setMarkers(locations) {
+        for (var i = 0; i < locations.length; i++) {
+          var vehicle = locations[i];
+          var acc = "";
+          var engine_on = parseInt(vehicle["engine_on"]);
+          if(engine_on == 0){
+            acc = "ACC OFF";
+          }else{
+            acc = "ACC ON";
+          }
+          var car_icon = "off0.png";
+          var dir = parseFloat(vehicle['direction']);
 
-        if(dir < 22.5 || dir >= 337.5 ){
-          if(engine_on == 1) {
-            car_icon = "0.png";
-          }else{
-            car_icon = "off0.png";
+          if(dir < 22.5 || dir >= 337.5 ){
+            if(engine_on == 1) {
+              car_icon = "0.png";
+            }else{
+              car_icon = "off0.png";
+            }
+          }else if(dir >= 22.5 && dir < 67.5 ){
+            if(engine_on == 1) {
+              car_icon = "45.png";
+            }else{
+              car_icon = "off45.png";
+            }
+          }else if((dir >= 67.5) && (dir < 112.5)){
+            if(engine_on == 1) {
+              car_icon = "90.png";
+            }else{
+              car_icon = "off90.png";
+            }
+          }else if((dir >= 112.5) && (dir < 157.5)){
+            if(engine_on == 1) {
+              car_icon = "135.png";
+            }else{
+              car_icon = "off135.png";
+            }
+          }else if((dir >= 157.5) && (dir < 202.5)){
+            if(engine_on == 1) {
+              car_icon = "180.png";
+            }else{
+              car_icon = "off180.png";
+            }
+          }else if((dir >= 202.5) && (dir < 247.5)){
+            if(engine_on == 1) {
+              car_icon = "225.png";
+            }else{
+              car_icon = "off225.png";
+            }
+          }else if((dir >= 247.5) && (dir < 292.5)){
+            if(engine_on == 1) {
+              car_icon = "270.png";
+            }else{
+              car_icon = "off270.png";
+            }
+          }else if((dir >= 292.5) && (dir < 337.5)){
+            if(engine_on == 1) {
+              car_icon = "315.png";
+            }else{
+              car_icon = "off315.png";
+            }
           }
-        }else if(dir >= 22.5 && dir < 67.5 ){
-          if(engine_on == 1) {
-            car_icon = "45.png";
-          }else{
-            car_icon = "off45.png";
-          }
-        }else if((dir >= 67.5) && (dir < 112.5)){
-          if(engine_on == 1) {
-            car_icon = "90.png";
-          }else{
-            car_icon = "off90.png";
-          }
-        }else if((dir >= 112.5) && (dir < 157.5)){
-          if(engine_on == 1) {
-            car_icon = "135.png";
-          }else{
-            car_icon = "off135.png";
-          }
-        }else if((dir >= 157.5) && (dir < 202.5)){
-          if(engine_on == 1) {
-            car_icon = "180.png";
-          }else{
-            car_icon = "off180.png";
-          }
-        }else if((dir >= 202.5) && (dir < 247.5)){
-          if(engine_on == 1) {
-            car_icon = "225.png";
-          }else{
-            car_icon = "off225.png";
-          }
-        }else if((dir >= 247.5) && (dir < 292.5)){
-          if(engine_on == 1) {
-            car_icon = "270.png";
-          }else{
-            car_icon = "off270.png";
-          }
-        }else if((dir >= 292.5) && (dir < 337.5)){
-          if(engine_on == 1) {
-            car_icon = "315.png";
-          }else{
-            car_icon = "off315.png";
-          }
-        }
-        if(check_checked(vehicle['VNO'])){
-          var myLatLng = new google.maps.LatLng(vehicle["latitude"], vehicle["longitude"]);
-          var title = vehicle["VNO"] + "\n" + vehicle["terminal_id"] + "\n" + vehicle["ground_speed"]; 
-          var marker = new google.maps.Marker({
+          if(check_checked(vehicle['VNO'])){
+            var myLatLng = new google.maps.LatLng(vehicle["latitude"], vehicle["longitude"]);
+            var title = vehicle["VNO"] + "\n" + vehicle["terminal_id"] + "\n" + vehicle["ground_speed"]; 
+            var marker = new google.maps.Marker({
               position: myLatLng,
               map: mapnormal,
               icon: car_icon,
               animation: google.maps.Animation.NONE,
               title: title
-          });
-          markers.push(marker);
+            });
+            markers.push(marker);
+          }
         }
-    }
-}
-function reloadMarkers() {
-    for (var i=0; i<markers.length; i++) {
-        markers[i].setMap(null);
-    }
-    markers = [];
-    $.ajax({
-      type: "get",
-      url: '{{ route('locations') }}',
-      success: function(response) {
-        vehicles = response;
-        setMarkers(vehicles);
-      },
-      error: function (jqXHR, exception) {
-        console.log(exception);
       }
-    });
-}
-function initialize() {
-    var mapOptions = {
-        zoom: 10,
-        center: new google.maps.LatLng(5.604688667802131, -0.187207828880368),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    mapnormal = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-    setInterval(reloadMarkers, 20000);
-}
-initialize();
+      function reloadMarkers() {
+        for (var i=0; i<markers.length; i++) {
+          markers[i].setMap(null);
+        }
+        markers = [];
+        $.ajax({
+          type: "get",
+          url: '{{ route('locations') }}',
+          success: function(response) {
+            vehicles = response;
+            setMarkers(vehicles);
+          },
+          error: function (jqXHR, exception) {
+            console.log(exception);
+          }
+        });
+      }
+      function initialize() {
+        var mapOptions = {
+          zoom: 10,
+          center: new google.maps.LatLng(5.604688667802131, -0.187207828880368),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        mapnormal = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+        setInterval(reloadMarkers, 20000);
+      }
+      initialize();
 
-$(document).ready(function(){
-  $('#examplegrid').dataTable({
-    searching: false, paging: false,
-    drawCallback: function() {
-      $('[data-toggle="popover"]').popover();
-    }  
-  });
+      $(document).ready(function(){
+        $('#examplegrid').dataTable({
+          searching: false, paging: false,
+          drawCallback: function() {
+            $('[data-toggle="popover"]').popover();
+          }  
+        });
 
-  $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
-});  
-</script>
+        $('#myModal').on('shown.bs.modal', function () {
+          $('#myInput').trigger('focus')
+        })
+      });  
+    </script>
 
-@endsection
+    @endsection
 
