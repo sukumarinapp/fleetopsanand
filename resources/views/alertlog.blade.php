d329ff71311433a2fc8607f05a38243d8fdb6067
sukumarinapp <sukumar.inapp@gmail.com>
2021-11-26 13:32:46 +0530

    alert log

--- resources/views/alertlog.blade.php
+++ resources/views/alertlog.blade.php
@@ -9,7 +9,7 @@
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
               <li class="breadcrumb-item"><a href="#">Reports</a></li>
-              <li class="breadcrumb-item">Notification Log</li>
+              <li class="breadcrumb-item">Alert Log</li>
             </ol>
           </div>
         </div>
