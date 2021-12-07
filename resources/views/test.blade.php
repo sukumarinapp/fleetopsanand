b83197f8835ed4a049aba923c0b0d0fd9bc62198
sukumarinapp <sukumar.inapp@gmail.com>
2021-12-07 12:26:14 +0530

    db dump

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -79,10 +79,10 @@
 		});
 	}
 
-	setInterval(animateMarkers, 10000);
+	setInterval(animateMarkers, 20000);
 
-	var numDeltas = 100;
-	var delay = 100;
+	var numDeltas = 200;
+	var delay = 200;
 	var i = 0;
 	var deltaLat=0.0;
 	var deltaLng=0.0;
