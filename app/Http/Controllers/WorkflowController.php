7211fd9c22283f42a1d3bf2852cd789d2c31b823
sukumarinapp <sukumar.inapp@gmail.com>
2021-12-07 14:15:00 +0530

    db dump

--- app/Http/Controllers/WorkflowController.php
+++ app/Http/Controllers/WorkflowController.php
@@ -99,7 +99,7 @@ class WorkflowController extends Controller
             }
             $sql = "select max(SPF) as SPF2,sum(RMT) as CPF2,max(TPF) as TPF2 from tbl137 where DCR=$DCR and SSR='Driver' and RST=1";
             $tbl137 = DB::select(DB::raw($sql));
-            if(count($tbl137) > 0 && $tbl137[0]->CPF2 != ""){
+            if(count($tbl137) > 0 && $tbl137[0]->CPF2){
                 $sale->EXPS = round(Formulae::EXPS2($DCR),2);
                 $sale->EXPS_EARNING = ($sale->EXPS * $RHF)/100;
                 $sale->CCEI = round(Formulae::CCEI2($DCR),2);
