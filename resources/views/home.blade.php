9baa2595026e8effbb6003c768169a0fa46433b1
sukumarinapp <sukumar.inapp@gmail.com>
2021-12-08 15:08:16 +0530

    add event location in alert log

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -333,7 +333,7 @@
               position: myLatLng,
               map: mapnormal,
               icon: car_icon,
-              animation: google.maps.Animation.NONE,
+              animation: google.maps.Animation.DROP,
               title: title
             });
             markers.push(marker);
