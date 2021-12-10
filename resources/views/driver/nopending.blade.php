b2689521379518b2a60430ed12957857b05d632b
universejino <universejino@gmail.com>
2021-12-10 17:25:17 +0530

    icon

--- resources/views/driver/nopending.blade.php
+++ resources/views/driver/nopending.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.driver')
+ @extends('layouts.driver')
 @section('content')
 <div class="container" >
   <div class="row justify-content-center">
   <div class="col-md-12 text-center">
-    <h3 style="color: lightgray">Declare Sales</h3>
+    <h3 style="color: lightgray">Collection Note</h3>
   </div>
 </div>
   <div class="card card-danger">
