0cd4f7b41e889a99fff13389cfb4bfb56d76f4cf
universejino <universejino@gmail.com>
2021-12-22 17:13:37 +0530

    login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -423,7 +423,7 @@
             acc = "ACC ON";
             car_icon = "yellowcar.jpg";
           }
-          if((driver_id == "") || isnull(driver_id)){
+          if((driver_id == "") || (driver_id === NULL)){
             car_icon = "bluecar.png";
           }
           console.log(car_icon);
