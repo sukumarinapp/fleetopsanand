6e9978c3fd4176d7cf432b48c2d678025ba49c84
sukumarinapp <sukumar.inapp@gmail.com>
2022-01-05 12:50:26 +0530

    vehicle status

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -310,31 +310,33 @@ class HomeController extends Controller
         $inactive = 0;
         $new = 0;
         $total = 0;
-        $sql = " select count(distinct terminal_id) as online from current_location where capture_date='$today'";
+
+        $sql = " select count(*) as total from vehicle";
         $result = DB::select(DB::raw($sql));
         if(count($result) > 0){
-          $online = $result[0]->online;
+          $total = $result[0]->total;
         }
-        $sql = " select count(VNO) as offline from vehicle where VTV=1 and driver_id <>'' and TID not in (select distinct terminal_id from current_location where capture_date='$today')";
+
+        $sql = " select count(*) as offline from tracker_status where status=0";
         $result = DB::select(DB::raw($sql));
         if(count($result) > 0){
           $offline = $result[0]->offline;
         }
+
+        $online = $total - $offline;        
+        
         $sql = "select count(VNO) as inactive from vehicle where VTV = 0 or driver_id is null";
         $result = DB::select(DB::raw($sql));
         if(count($result) > 0){
           $inactive = $result[0]->inactive;
         }
+
         $sql = " select count(VNO) as active from vehicle where VTV=1 and driver_id <> ''";
         $result = DB::select(DB::raw($sql));
         if(count($result) > 0){
           $active = $result[0]->active;
         }
-        $sql = " select count(VNO) as total from vehicle";
-        $result = DB::select(DB::raw($sql));
-        if(count($result) > 0){
-          $total = $result[0]->total;
-        }
+        
         $alerts = self::alerts();
         return view('home',compact('usertree','alerts','type','online','offline','inactive','active','total','starttime','endtime'));
       }
